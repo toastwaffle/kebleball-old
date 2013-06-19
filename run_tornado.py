7aee1bf224dbc36d895fb105d2b2db49a4404c05
Samuel Littley <samuel.littley@toastwaffle.com>
2013-06-19 22:55:56 +0100

    Add tornado run script

---
+++ run_tornado.py
@@ -0,0 +1,16 @@
+import os, sys
+
+dir = os.getcwd()
+
+activate_this = os.path.join(dir,'bin/activate_this.py')
+with open(activate_this, 'r') as fh:
+    exec(fh.read()+'\n', dict(__file__=activate_this), sys._getframe(0).f_locals)
+
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tickets import app
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
